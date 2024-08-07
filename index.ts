fde3024a9548507e90735b4b74bf3dafbdfeeaf3
ENvironmentSet <sbshw1@gmail.com>
2024-08-07 16:47:49 +0900

    add missing export

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import { TypeRep } from './TypeRepresentation.js';
 export declare function typeRep<T>(): TypeRep;
 export {
   TypeKind, TypeRep, NumberRep, BooleanRep, BigIntRep, StringRep, SymbolRep, NullRep, UndefinedRep,
-  VoidRep, AnyRep, UnknownRep, NeverRep, NonPrimitiveRep, UnionRep, IntersectionRep, ObjectRep
+  VoidRep, AnyRep, UnknownRep, NeverRep, NonPrimitiveRep, UnionRep, IntersectionRep, ObjectRep, FunctionRep
 } from './TypeRepresentation';
